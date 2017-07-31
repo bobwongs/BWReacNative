2367bef995d1ed443e66b76790eea0012e76d91c
BobWong <wangbo@bluemoon.com.cn>
2017-07-31 17:36:05 +0800

    Modify

---
+++ HybridApp/ios/BWHybridApp/BWReactNativeBridge.h
@@ -0,0 +1,13 @@
+//
+//  BWReactNativeBridge.h
+//  BWHybridApp
+//
+//  Created by BobWong on 2017/7/31.
+//  Copyright © 2017年 BobWongStudio. All rights reserved.
+//
+
+#import <React/React.h>
+
+@interface BWReactNativeBridge : RCTEventEmitter
+
+@end
---
+++ HybridApp/ios/BWHybridApp/BWReactNativeBridge.m
@@ -0,0 +1,13 @@
+//
+//  BWReactNativeBridge.m
+//  BWHybridApp
+//
+//  Created by BobWong on 2017/7/31.
+//  Copyright © 2017年 BobWongStudio. All rights reserved.
+//
+
+#import "BWReactNativeBridge.h"
+
+@implementation BWReactNativeBridge
+
+@end
